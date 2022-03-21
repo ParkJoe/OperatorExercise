8e8d83a6d8c8cd186c9c09dc1fb3eccc1f9c3488
Joe Park <joe_park@live.com>
2022-03-21 12:43:27 -0400

    initial commit 3/21/22

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create 2 integer variables
+            int a = 17;
+            int b = 4;
+
+            // Create 2 more integer variables
+            int quotient = a / b;
+            int remainder = a % b;
+
+            // Use an if statement
+            if (a == 17 && b ==4)
+            
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            
+
+
+
+
+
+            
+        }
+    }
+}
